e430b704b89540bc243cfe5b760884359d45d775
abradle <anthony.richard.bradley@gmail.com>
2016-07-09 17:53:10 -0400

    Update to the demo script and travis

--- src/demo.c
+++ src/demo.c
@@ -1,36 +1,33 @@
+#include <stdlib.h>
 #include <msgpack.h>
-#include <stdio.h>
+
+static const int BUFFERSIZE = 2048;
 
 int main(void)
 {
-    /* msgpack::sbuffer is a simple buffer implementation. */
-    msgpack_sbuffer sbuf;
-    msgpack_sbuffer_init(&sbuf);
-
-    /* serialize values into the buffer using msgpack_sbuffer_write callback function. */
-    msgpack_packer pk;
-    msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
-
-    msgpack_pack_array(&pk, 3);
-    msgpack_pack_int(&pk, 1);
-    msgpack_pack_true(&pk);
-    msgpack_pack_str(&pk, 7);
-    msgpack_pack_str_body(&pk, "example", 7);
-
-    /* deserialize the buffer into msgpack_object instance. */
-    /* deserialized object is valid during the msgpack_zone instance alive. */
-    msgpack_zone mempool;
-    msgpack_zone_init(&mempool, 2048);
-
-    msgpack_object deserialized;
-    msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
-
-    /* print the deserialized object. */
-    msgpack_object_print(stdout, deserialized);
-    puts("");
-
-    msgpack_zone_destroy(&mempool);
-    msgpack_sbuffer_destroy(&sbuf);
-
+    char *inbuffer = (char *) malloc(BUFFERSIZE);
+    if(NULL == inbuffer) {
+            fprintf(stderr, "Out of memory!");
+            return 1;
+    }
+    FILE *fp = fopen("../data/4HHB.mmtf", "rb");
+    size_t off = 0;
+    size_t read = 0;
+    msgpack_unpacked unpacked;
+    msgpack_unpacked_init(&unpacked);
+    do {
+            read = fread(inbuffer, sizeof(char), BUFFERSIZE - off, fp);
+            off = 0;
+            while(msgpack_unpack_next(&unpacked, inbuffer, read, &off)) {
+                    msgpack_object_print(stdout, unpacked.data);
+                    puts("");
+            }
+            memcpy(inbuffer, &(inbuffer[off]), read-off);
+            off = read - off;
+    } while(read != 0);
+    msgpack_object_print(stdout, unpacked.data);
+    free(inbuffer);
+    fclose(fp);
+    msgpack_unpacked_destroy(&unpacked);
     return 0;
 }
