977b547bc1c324a80147ba67835a819c473516e9
abradle <anthony.richard.bradley@gmail.com>
2016-07-13 07:31:31 -0700

    Added check and msgpack sources direcrtly - for CI for now

---
+++ check-0.10.0/check_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _CHECK_CHECK_STDINT_H
+#define _CHECK_CHECK_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "check 0.10.0"
+/* generated using gnu compiler Apple LLVM version 6.1.0 (clang-602.0.53) (based on LLVM 3.6.0svn) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
