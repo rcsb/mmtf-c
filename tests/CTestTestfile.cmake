f0b6d83bd7c954791c9fe6942005ce59b8036037
abradle <anthony.richard.bradley@gmail.com>
2016-09-10 20:32:15 -0700

    Remove test and CI - we should rethink this now

--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julien/Desktop/MutaFrame/Hackathon/mmtf-c/tests
-# Build directory: /home/julien/Desktop/MutaFrame/Hackathon/mmtf-c/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_num_converters "/home/julien/Desktop/MutaFrame/Hackathon/mmtf-c/tests/test_num_converters")
