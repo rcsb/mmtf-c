4e3fdb7bf6a65dbf61277c800889fd191dbdc1f5
abradle <anthony.richard.bradley@gmail.com>
2016-09-29 19:08:52 -0700

    Me

--- src/install-deps.sh
+++ src/install-deps.sh
@@ -1,10 +1,7 @@
-
-
 export CXXFLAGS="-w -Wno-error -Wno-unknown-pragmas ${CXXFLAGS}";
 export CFLAGS="-w -Wno-error -Wno-error=format-security ${CFLAGS}";
-
 git clone https://github.com/msgpack/msgpack-c
 cd msgpack-c
 cmake .
-make CFLAGS = "-Wno-error -Wno-error=format-security"
+make
 sudo make install
