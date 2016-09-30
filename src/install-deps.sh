00eee8185d6cebf2408b8d2e347207f9e8ad8326
abradle <anthony.richard.bradley@gmail.com>
2016-09-29 19:02:08 -0700

    Nuclear

--- src/install-deps.sh
+++ src/install-deps.sh
@@ -1,7 +1,7 @@
 
 
-export CXXFLAGS="-Wno-error -Wno-unknown-pragmas ${CXXFLAGS}";
-export CFLAGS="-Wno-error -Wno-error=format-security ${CFLAGS}";
+export CXXFLAGS="-w -Wno-error -Wno-unknown-pragmas ${CXXFLAGS}";
+export CFLAGS="-w -Wno-error -Wno-error=format-security ${CFLAGS}";
 
 git clone https://github.com/msgpack/msgpack-c
 cd msgpack-c
