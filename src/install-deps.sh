fd0ec0b1117d4a31c2fb37dd0791272f4d9aa838
abradle <anthony.richard.bradley@gmail.com>
2016-09-29 18:41:16 -0700

    Fix for sh

--- src/install-deps.sh
+++ src/install-deps.sh
@@ -1,8 +1,8 @@
 
-CFLAGS = "-Wno-error -Wno-error=format-security"
+CFLAGS="-Wno-error -Wno-error=format-security"
 
 git clone https://github.com/msgpack/msgpack-c
 cd msgpack-c
 cmake .
-make
+make CFLAGS = "-Wno-error -Wno-error=format-security"
 sudo make install
