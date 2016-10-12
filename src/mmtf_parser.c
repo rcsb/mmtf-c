01ad22f8b26896e650ee65af255380b568d9f2f3
Yana Valasatava <yana.valasatava@rcsb.org>
2016-10-12 12:31:39 -0700

    Apache 2.0 licencing and authors/contributors list are added to MMTF parser.

--- src/mmtf_parser.c
+++ src/mmtf_parser.c
@@ -1,18 +1,28 @@
-//*******************************************************************************
-//******	This code is published under the .... license
+//***************************************************************************************************
+//******	Copyright [2016] [RCSB]
+//******
+//******	Licensed under the Apache License, Version 2.0 (the "License");
+//******	you may not use this file except in compliance with the License.
+//******	You may obtain a copy of the License at
+//******
+//******	    http://www.apache.org/licenses/LICENSE-2.0
+//******
+//******	Unless required by applicable law or agreed to in writing, software
+//******	distributed under the License is distributed on an "AS IS" BASIS,
+//******	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//******	See the License for the specific language governing permissions and
+//******	limitations under the License.
 //******
 //******
-//******	This file is the code file for the MMTF parser for the C language.
+//******	This file is mmtf_parser.c, holding the source code of the MMTF parser
+//******	for the C language.
 //******
-//******	The authors of this code are: Julien Fert√©, Thomas Holder, ...
+//******	The authors of this code are Julien Ferté (http://www.julienferte.com/),
+//******	Anthony Bradley, Thomas Holder.
 //******
+//******	Other contributors: Yana Valasatava, Alexander Rose.
 //******
-//******
-//******
-//******
-//******
-//******
-//*******************************************************************************
+//************************************************************************************************
 
 #include "mmtf_parser.h"
 
