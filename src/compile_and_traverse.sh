f7d689eb5b11a69d18b4a907361a75c8e3aab872
Yana Valasatava <yana.valasatava@rcsb.org>
2016-12-06 19:56:13 -0800

    Make traverse like pdb a default

--- src/compile_and_traverse.sh
+++ src/compile_and_traverse.sh
@@ -3,8 +3,8 @@
 gcc -c mmtf_parser.c &&
 gcc -c traverse.c &&
 
-gcc -o a.run mmtf_parser.o traverse.o -lmsgpackc &&
+gcc -o traverse.exe mmtf_parser.o traverse.o -lmsgpackc &&
 
 rm *.o
 
-#./a.run $1
+./traverse.exe $1
