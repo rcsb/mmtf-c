0a896572c3bce7f4b944426e070d47fcbfbd7800
Yana Valasatava <yana.valasatava@rcsb.org>
2016-12-15 16:52:40 -0800

    generating docs

---
+++ doc/html/structMMTF__GroupType.js
@@ -0,0 +1,15 @@
+var structMMTF__GroupType =
+[
+    [ "atomNameList", "structMMTF__GroupType.html#ac802938fab10a6b4c55bc6e318785789", null ],
+    [ "atomNameListCount", "structMMTF__GroupType.html#a6229e59dcd0d9c833d7274040068a2bd", null ],
+    [ "bondAtomList", "structMMTF__GroupType.html#aa06c97959e6264775e9e002bc8d89495", null ],
+    [ "bondAtomListCount", "structMMTF__GroupType.html#ab42392f34d6cf5e438a94bafc041a15d", null ],
+    [ "bondOrderList", "structMMTF__GroupType.html#ad5e48e2dd56747bdf9208274553ecea5", null ],
+    [ "bondOrderListCount", "structMMTF__GroupType.html#a65cd2c0845be003358608517311141d4", null ],
+    [ "chemCompType", "structMMTF__GroupType.html#acbdc724a38e5ac5bc4b1d2c572b265c5", null ],
+    [ "elementList", "structMMTF__GroupType.html#a6a0b040e9c4b091ab3c967d2d46ff777", null ],
+    [ "elementListCount", "structMMTF__GroupType.html#af7f21ca1ef6d159a9797e5519f4e3ea2", null ],
+    [ "formalChargeList", "structMMTF__GroupType.html#a38ff21ce512e02e10a20553772afec5f", null ],
+    [ "groupName", "structMMTF__GroupType.html#a61da8307a67c0ef019978d68770d8d19", null ],
+    [ "singleLetterCode", "structMMTF__GroupType.html#aca18d19000a45e2e3c0dd7c530d9d644", null ]
+];
