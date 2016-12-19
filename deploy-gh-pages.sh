a88e6221776947a0dd7a01e2ab339d5d6af3ee86
Yana Valasatava <yana.valasatava@rcsb.org>
2016-12-19 11:27:55 -0800

    deploy-gh-pages.sh is updated

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -14,6 +14,6 @@ git checkout gh-pages
 if [ -d "docs/api/"$version ]; then rm -Rf docs/api/$version; fi
 git checkout master  docs/api/$version
 git add docs/api/$version
-git commit "documentation is updated to "$version -- docs/api/$version
+git commit -m "documentation is updated to "$version -- docs/api/$version
 git push origin gh-pages
 git checkout master
