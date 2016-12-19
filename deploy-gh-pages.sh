e5e8fdac3208d8021fd7d3b3d3eea894921b7985
Yana Valasatava <yana.valasatava@rcsb.org>
2016-12-19 11:02:25 -0800

    deploy-gh-pages.sh is updated

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,14 +6,14 @@ cd docs
 if [ -d "api/"$version ]; then rm -Rf api/$version; fi
 doxygen Doxyfile
 git add api/$version
-git commit –m "documentation is generated for "$version -- api/$version
+git commit -m "documentation is generated for "$version
 git push origin master
 cd ..
 
 git checkout gh-pages
-rm -r docs/api/$version/*
-git checkout master — docs/api/$version
+if [ -d "docs/api/"$version ]; then rm -Rf docs/api/$version; fi
+git checkout master  docs/api/$version
 git add docs/api/$version
-git commit “documentation is updated to ”$version -- docs/api/$version
+git commit "documentation is updated to "$version -- docs/api/$version
 git push origin gh-pages
 git checkout master
