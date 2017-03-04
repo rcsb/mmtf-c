d79eb9c9ec88f7d14040e0d65d3bdf28ef6e2ecf
Yana Valasatava <yana.valasatava@rcsb.org>
2017-03-03 19:45:11 -0800

    test

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,8 +2,23 @@
 
 version=$1
 
+if [ ! -d "$docs" ]; then
+mkdir docs
+fi
+
+if [ ! -d "$docs/api" ]; then
+mkdir docs/api
+fi
+
+if [ -d "docs/api/"$version ]; then rm -Rf docs/api/$version; fi
+
+doxygen Doxyfile
+
+git checkout gh-pages
 git checkout master  docs/api/$version
 git add docs/api/$version
 git commit -m "documentation is updated to "$version -- docs/api/$version
-git push origin master:gh-pages
+git push origin gh-pages
 git checkout master
+
+rm -r docs
