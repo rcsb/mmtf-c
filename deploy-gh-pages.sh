6597b30757999b7d9e8313522baa9887506d2b2d
Yana Valasatava <yana.valasatava@rcsb.org>
2017-03-03 19:02:58 -0800

    deploy documentation to build directory

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,13 +2,13 @@
 
 version=$1
 
-cd docs
+cd build/docs
 if [ -d "api/"$version ]; then rm -Rf api/$version; fi
 doxygen Doxyfile
 git add api/$version
 git commit -m "documentation is generated for "$version
 git push origin master
-cd ..
+cd ../..
 
 git checkout gh-pages
 if [ -d "docs/api/"$version ]; then rm -Rf docs/api/$version; fi
